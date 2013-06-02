d3f948c2ffe719fac4685f05cd27068081c30843
Jose Antonio Rodríguez <jarf1975@gmail>
2013-06-03 00:30:33 +0200

    Django tutorial 3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = 'Europe/Madrid'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-gb'
 
 SITE_ID = 1
 
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    './templates'
 )
 
 INSTALLED_APPS = (
@@ -121,7 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
